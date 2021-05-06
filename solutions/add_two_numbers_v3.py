744d32c4458804d3e03cd49b6c033c7b6536f188
CHOI HEE JAE <gmlwo530@gmail.com>
2021-05-07 00:56:14 +0900

    Rename files

--- solutions/add_two_numbers_v3.py
+++ solutions/add_two_numbers_v3.py
@@ -5,19 +5,44 @@ class ListNode:
 
 
 class Solution:
-    def addTwoNumbers(self, l1: ListNode, l2: ListNode, c: int = 0) -> ListNode:
-        val = l1.val + l2.val + c
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        result_list_node = None
+        prev_list_node = None
 
-        ret = ListNode(val % 10)
+        carry = 0
 
-        c = val // 10
+        while True:
+            if l1 is None and l2 is None:
+                if carry > 0:
+                    prev_list_node.next = ListNode(val=carry)
+                    carry = 0
+                    continue
+                else:
+                    break
 
-        if l1.next or l2.next or c:
-            if not l1.next:
-                l1.next = ListNode(0)
-            if not l2.next:
-                l2.next = ListNode(0)
+            if l1 is None:
+                l1_val = 0
+            else:
+                l1_val = l1.val
 
-            ret.next = self.addTwoNumbers(l1.next, l2.next, c)
+            if l2 is None:
+                l2_val = 0
+            else:
+                l2_val = l2.val
 
-        return ret
+            val = (l1_val + l2_val + carry) % 10
+
+            if prev_list_node:
+                next_list_node = ListNode(val=val)
+                prev_list_node.next = next_list_node
+                prev_list_node = next_list_node
+            else:
+                result_list_node = ListNode(val=val)
+                prev_list_node = result_list_node
+
+            carry = (l1_val + l2_val + carry) // 10
+
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+
+        return result_list_node
